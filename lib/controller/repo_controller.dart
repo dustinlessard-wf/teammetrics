66f4fddb3397745d4e8aa3c99983b5647482cd61
Dustin Lessard <dustin.lessard@workiva.com>
2018-01-22 18:56:38 -0500

    updated!

--- lib/controller/repo_controller.dart
+++ lib/controller/repo_controller.dart
@@ -9,13 +9,15 @@ class RepoController extends HTTPController {
 
   RepoController() : super() {
     responseContentType = ContentType.HTML;
+    Logger.root.onRecord.listen(
+        (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
   }
 
   String generateTableHtmlStart(List<String> people) {
     String peopleHtml = '';
     people.forEach((String name) {
       peopleHtml +=
-      '<th style="-webkit-transform: rotate(-90deg); font-size: 10px; height:100px;"><span style="display:inline-block; height:80px;">$name</span></th>';
+          '<th style="-webkit-transform: rotate(-90deg); font-size: 10px; height:100px;"><span style="display:inline-block; height:80px;">$name</span></th>';
     });
     return '<table class="table table-bordered" style="margin:20px; width:80%;"><tr><th style="height:50px;font-size: 10px;"></th>$peopleHtml';
   }
@@ -23,12 +25,15 @@ class RepoController extends HTTPController {
   /// Get ContributorStatistics for repos
   Future<List<List<git.ContributorStatistics>>> getStatsForRepos(
       List<String> repos, String token) async {
-
-    final github = git.createGitHubClient(
-        auth: new git.Authentication.withToken(
-            token));
-
+    final github =
+        git.createGitHubClient(auth: new git.Authentication.withToken(token));
+    Logger.root.info('created client');
     List<Future<List<git.ContributorStatistics>>> asyncWork = [];
+
+//    github.repositories
+//          .listContributorStats(new git.RepositorySlug('Workiva', 'app_intelligence_dart')).then((List<git.ContributorStatistics> things){
+//       print('got something');
+//    });
 
     repos.forEach((String repoName) {
       print('adding work for $repoName');
@@ -36,7 +41,10 @@ class RepoController extends HTTPController {
           .listContributorStats(new git.RepositorySlug('Workiva', repoName)));
     });
 
-    return Future.wait(asyncWork);
+    return Future.wait(asyncWork).catchError((e, s) {
+      print(e);
+      print(s);
+    });
   }
 
   Map<String, dynamic> generateRepoAndPeopleMaps(
@@ -46,29 +54,31 @@ class RepoController extends HTTPController {
     int repoListIndex = 0;
     Map<String, int> peopleToCommits = {};
     Map<String, Map<String, int>> repoToPeople = {};
-    statsList.forEach((List<git.ContributorStatistics> stats) {
-      String repoName = repoList[repoListIndex];
-      // process each repo's contributors
-      peopleToCommits = {};
-      people.forEach((String name) {
-        peopleToCommits[name] = 0;
-      });
-      stats.forEach((git.ContributorStatistics stat) {
-        if (people.contains(stat.author.login)) {
-          peopleToCommits[stat.author.login] = stat.total;
-        }
-      });
-      print('have stats for $repoName');
-      repoToPeople[repoName] = peopleToCommits;
+    if (statsList != null) {
+      statsList.forEach((List<git.ContributorStatistics> stats) {
+        String repoName = repoList[repoListIndex];
+        // process each repo's contributors
+        peopleToCommits = {};
+        people.forEach((String name) {
+          peopleToCommits[name] = 0;
+        });
+        stats.forEach((git.ContributorStatistics stat) {
+          if (people.contains(stat.author.login)) {
+            peopleToCommits[stat.author.login] = stat.total;
+          }
+        });
+        print('have stats for $repoName');
+        repoToPeople[repoName] = peopleToCommits;
 
-      repoListIndex++;
-    });
-
+        repoListIndex++;
+      });
+    }
     return {'repoToPeople': repoToPeople, 'peopleToCommits': peopleToCommits};
   }
 
   String generateRepoToPeopleHtml(Map<String, Map<String, int>> repoToPeople,
       Map<String, int> peopleToCommits) {
+    print('generateRepoToPeopleHtml');
     String repohtml = '';
     repoToPeople.forEach((String repoName, dynamic peopleToCommits) {
       repohtml += '<tr><th style="font-size: 10px;">$repoName</th>';
@@ -82,7 +92,7 @@ class RepoController extends HTTPController {
           color = 'green';
         }
         repohtml +=
-        '<td style="background-color:$color; width:10px; height:10px;" alt="$value">&nbsp;</td>';
+            '<td style="background-color:$color; width:10px; height:10px;" alt="$value">&nbsp;</td>';
       });
       repohtml += '</tr>';
     });
@@ -90,17 +100,17 @@ class RepoController extends HTTPController {
   }
 
   @httpGet
-  Future<Response> getRepo(@HTTPQuery("repos") String repos,
+  Future<Response> getRepo(
+      @HTTPQuery("repos") String repos,
       @HTTPQuery("authors") String authors,
       @HTTPQuery("token") String token) async {
-
     List<String> people = authors.split(',');
     List<String> repoList = repos.split(',');
     List<List<git.ContributorStatistics>> statsList =
-    await getStatsForRepos(repoList, token);
+        await getStatsForRepos(repoList, token);
 
     Map<String, dynamic> repoAndPeopleMaps =
-    generateRepoAndPeopleMaps(statsList, people, repoList);
+        generateRepoAndPeopleMaps(statsList, people, repoList);
 
     print(repoAndPeopleMaps);
 
@@ -112,4 +122,3 @@ class RepoController extends HTTPController {
         '<html>${headHtml}<body><h1>People and Repos!</h1>${generateTableHtmlStart(people)}</tr>${repoHtml}</table></body></html>');
   }
 }
-
