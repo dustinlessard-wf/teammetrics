6fb78e826d909f558f35fcc70a5a1ac6a7f5c84d
Dustin Lessard <dustin.lessard@workiva.com>
2018-01-22 19:16:21 -0500

    updated!

--- lib/controller/pr_controller.dart
+++ lib/controller/pr_controller.dart
@@ -9,50 +9,51 @@ class PrController extends HTTPController {
   }
 
   @httpGet
-  Future<Response> getPrs(@HTTPQuery("repos") String repos, {@HTTPQuery("limit") int limit : 10,
-    @HTTPQuery("ignoreAuthors") String ignoreAuthors: ''}) async{
+  Future<Response> getPrs(@HTTPQuery("repos") String repos,
+      {@HTTPQuery("token") String token,
+      @HTTPQuery("ignoreAuthors") String ignoreAuthors: ''}) async {
     List<String> ignoreAuthorsList = ignoreAuthors.split(',');
-    final github = git.createGitHubClient(
-        auth: new git.Authentication.withToken(
-            'cd242d7cfed7230274fef75b9669a7d107fde02d'));
+    final github =
+        git.createGitHubClient(auth: new git.Authentication.withToken(token));
     List<String> repoList = repos.split(",");
     List<git.PullRequest> prs = [];
     for (var repoName in repoList) {
       final repoSlug = new git.RepositorySlug("workiva", repoName);
-      final rawPullRequests = github.pullRequests
-          .list(repoSlug);
+      final rawPullRequests = github.pullRequests.list(repoSlug);
       List<git.PullRequest> pullRequests = await rawPullRequests.toList();
 
-      prs.addAll(pullRequests.where((git.PullRequest pr){
-        return pr.state == 'open' && ignoreAuthorsList.indexOf(pr.user.login) == -1;
+      prs.addAll(pullRequests.where((git.PullRequest pr) {
+        return pr.state == 'open' &&
+            ignoreAuthorsList.indexOf(pr.user.login) == -1;
       }));
     }
 
-    prs.sort((git.PullRequest a, git.PullRequest b){
+    prs.sort((git.PullRequest a, git.PullRequest b) {
       return a.createdAt.compareTo(b.createdAt);
     });
 
     String prHtml = '';
-    String headHtml = '<head><link rel="stylesheet" href="https://cdn.wdesk.com/home/3.0.13/packages/web_skin/dist/css/web-skin.min.css"></head>';
+    String headHtml =
+        '<head><link rel="stylesheet" href="https://cdn.wdesk.com/home/3.0.13/packages/web_skin/dist/css/web-skin.min.css"></head>';
 
     String panelclass = 'panel-warning';
     DateTime now = new DateTime.now();
-    prs.forEach((git.PullRequest pr){
+    prs.forEach((git.PullRequest pr) {
       int days = now.difference(pr.createdAt).inDays;
       String age = ' is $days days old!!';
-      if(days >= 7){
+      if (days >= 7) {
         panelclass = 'panel-danger';
       }
-      if(days <= 2){
+      if (days <= 2) {
         panelclass = 'panel-success';
         age = '';
       }
 
-
-      prHtml += '<div class="panel ${panelclass}"><div class="panel-heading">${pr.title}$age</div><div class="panel-body"><label>created at:</label> ${pr.createdAt}<br/><label>url:</label> <a target="_blank" href="${pr.htmlUrl}">${pr.htmlUrl}</a></div></div>';
-
+      prHtml +=
+          '<div class="panel ${panelclass}"><div class="panel-heading">${pr.title}$age</div><div class="panel-body"><label>created at:</label> ${pr.createdAt}<br/><label>url:</label> <a target="_blank" href="${pr.htmlUrl}">${pr.htmlUrl}</a></div></div>';
     });
 
-    return new Response.ok('<html>${headHtml}<body><h1>Pull Requests!</h1>${prHtml}</body></html>');
+    return new Response.ok(
+        '<html>${headHtml}<body><h1>Pull Requests!</h1>${prHtml}</body></html>');
   }
 }
