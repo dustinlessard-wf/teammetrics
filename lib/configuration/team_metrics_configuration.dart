6256c211b1eed4e286266038937d07fb28346e30
Joel Leibow <joel.leibow@workiva.com>
2018-02-10 14:05:26 -0700

    Name consistent with project

--- lib/configuration/team_metrics_configuration.dart
+++
@@ -1,9 +0,0 @@
-import 'package:aqueduct/aqueduct.dart';
-
-import './github_configuration.dart';
-
-class TeamMetricsConfiguration extends ConfigurationItem {
-  TeamMetricsConfiguration(String path) : super.fromFile(path);
-
-  GithubConfiguration github;
-}
