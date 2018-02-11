a1368c27fa037363efdadc249382e4396631b14f
Joel Leibow <joel.leibow@workiva.com>
2018-02-11 14:35:04 -0700

    Do not use https for MockHttpServer as it connects via bind, not bindSecure.

--- lib/controller/pr_controller.dart
+++ lib/controller/pr_controller.dart
@@ -15,49 +15,48 @@ class PrController extends HTTPController {
 
   @httpGet
   Future<Response> getPrs(@HTTPQuery("repos") String repos, { @HTTPQuery("ignoreAuthors") String ignoreAuthors: '' }) async {
-    // List<String> ignoreAuthorsList = ignoreAuthors.split(',');
+    List<String> ignoreAuthorsList = ignoreAuthors.split(',');
     final github = githubService.client;
       
     List<String> repoList = repos.split(",");
-    // List<git.PullRequest> prs = [];
+    List<git.PullRequest> prs = [];
+    
     for (var repoName in repoList) {
       final repoSlug = new git.RepositorySlug("workiva", repoName);
       final rawPullRequests = github.pullRequests.list(repoSlug);
-      // print(rawPullRequests);
       List<git.PullRequest> pullRequests = await rawPullRequests.toList();
-      print(pullRequests);
-
-    //   prs.addAll(pullRequests.where((git.PullRequest pr) {
-    //     return pr.state == 'open' &&
-    //         ignoreAuthorsList.indexOf(pr.user.login) == -1;
-    //   }));
+      
+      prs.addAll(pullRequests.where((git.PullRequest pr) {
+        return pr.state == 'open' &&
+            ignoreAuthorsList.indexOf(pr.user.login) == -1;
+      }));
     }
 
-    // prs.sort((git.PullRequest a, git.PullRequest b) {
-    //   return a.createdAt.compareTo(b.createdAt);
-    // });
+    prs.sort((git.PullRequest a, git.PullRequest b) {
+      return a.createdAt.compareTo(b.createdAt);
+    });
 
     String prHtml = '';
     String headHtml =
         '<head><link rel="stylesheet" href="https://cdn.wdesk.com/home/3.0.13/packages/web_skin/dist/css/web-skin.min.css"></head>';
 
     String panelclass = 'panel-warning';
-    // DateTime now = new DateTime.now();
-    // prs.forEach((git.PullRequest pr) {
-    //   int days = now.difference(pr.createdAt).inDays;
-    //   String age = ' is $days days old!!';
-    //   if (days >= 7) {
-    //     panelclass = 'panel-danger';
-    // }
+    DateTime now = new DateTime.now();
+    prs.forEach((git.PullRequest pr) {
+      int days = now.difference(pr.createdAt).inDays;
+      String age = ' is $days days old!!';
+      if (days >= 7) {
+        panelclass = 'panel-danger';
+    }
 
-    // if (days <= 2) {
-    //   panelclass = 'panel-success';
-    //   age = '';
-    // }
+    if (days <= 2) {
+      panelclass = 'panel-success';
+      age = '';
+    }
 
-    // prHtml +=
-    //   '<div class="panel ${panelclass}"><div class="panel-heading">${pr.title}$age</div><div class="panel-body"><label>created at:</label> ${pr.createdAt}<br/><label>url:</label> <a target="_blank" href="${pr.htmlUrl}">${pr.htmlUrl}</a></div></div>';
-    // });
+    prHtml +=
+      '<div class="panel ${panelclass}"><div class="panel-heading">${pr.title}$age</div><div class="panel-body"><label>created at:</label> ${pr.createdAt}<br/><label>url:</label> <a target="_blank" href="${pr.htmlUrl}">${pr.htmlUrl}</a></div></div>';
+    });
 
     return new Response.ok(
       '<html>${headHtml}<body><h1>Pull Requests!</h1>${prHtml}</body></html>');
